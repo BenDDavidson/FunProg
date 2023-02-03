8ef027490187758bf716115b8963f28f2ee02cc5
BenD <61838571+BenDDavidson@users.noreply.github.com>
2023-02-03 03:18:41 +0000

    all examples done

--- work/1.hs
+++ work/1.hs
@@ -2,3 +2,12 @@ circumferenceOfCircle :: Float -> Float
 circumferenceOfCircle d = pi * d
 
 sideOfCylinder :: Float -> Float -> Float
+sideOfCylinder d h = circumferenceOfCircle d * h
+
+canDrink :: Int -> Bool
+canDrink age = age >= 18
+
+all3CanDrink :: Int -> Int -> Int -> Bool
+all3CanDrink a b c = canDrink a && canDrink b && canDrink c
+
+
